5b4d2e42463647f03a525dcefda3a3357db6c8bd
Stanislavych <stasik.bartosh@mail.ru>
2023-08-04 01:51:37 +0300

    refactoring

--- PetFood.BusinessLogic/Dto/FoodItemDto.cs
+++ PetFood.BusinessLogic/Dto/FoodItemDto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
         public int PetId { get; set; }
         public int FoodTypeId { get; set; }
